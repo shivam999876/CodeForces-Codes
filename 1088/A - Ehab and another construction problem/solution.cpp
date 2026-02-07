ab0e0198021028c274d8980d60de37fb1f0ce861
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-07 20:13:09 +0530

    Add Ehab and another construction problem [A] from Codeforces

---
+++ 1088/A - Ehab and another construction problem/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve() {    int n;    cin >> n;    if (n == 1) cout << "-1";    else cout << n << ' ' << n;} signed main(void){        std::ios::sync_with_stdio(false);    cin.tie(0); cout.tie(0);        int t = 1;     // cin >> t;    while (t--) solve();    return 0;    }
