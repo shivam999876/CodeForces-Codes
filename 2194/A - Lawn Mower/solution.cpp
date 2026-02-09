767edd54485caa4e668c9888ab09a5148d9dff5f
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-09 13:37:40 +0530

    Add Lawn Mower [A] from Codeforces

---
+++ 2194/A - Lawn Mower/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std;void solve() {    int a, b;    cin >> a >> b;    cout << a / b * (b - 1) + a % b << '\n';} signed main(void){      std::ios::sync_with_stdio(false);    cin.tie(0); cout.tie(0);        int t = 1;     cin >> t;    while (t--) solve();    return 0;    }
