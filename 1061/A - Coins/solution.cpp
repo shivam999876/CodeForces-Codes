db828ce3328dd64d683fd52ec6e3d1df5592dd44
SHIVAM KUMAR <93633751+shivam999876@users.noreply.github.com>
2026-02-10 20:53:17 +0530

    Add Coins [A] from Codeforces

---
+++ 1061/A - Coins/solution.cpp
@@ -0,0 +1,1 @@
+#include <bits/stdc++.h>using namespace std; void solve() {    int a, b;    cin >> a >> b;    if (b % a) cout << (b + a) / a << '\n';    else cout << b / a << '\n';} signed main(void){        std::ios::sync_with_stdio(false);    cin.tie(0); cout.tie(0);        int t = 1;     // cin >> t;    while (t--) solve();    return 0;    }
